d2d4eedc71d1becbcbb974cd1d4aaa1d2a7ef379
AquaOH <2863896505@qq.com>
2023-11-22 19:02:20 +0800

    Site updated: 2023-11-22 19:02:20

---
+++ live2d-widget/src/utils.js
@@ -0,0 +1,3 @@
+// build time: Wed Nov 22 2023 18:55:46 GMT+0800 (中国标准时间) 
+function r(r){return Array.isArray(r)?r[Math.floor(Math.random()*r.length)]:r}export default r;
+//rebuild by hexo-renderer-multi-next-markdown-it 
